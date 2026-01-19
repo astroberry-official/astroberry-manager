5ad3f628be3df6dfb083fe78648444a9ef527577
Radek Kaczorek <rkaczorek@gmail.com>
2026-01-19 01:09:19 +0100

    Improve dependencies and requirements

--- dependencies.sh
+++ dependencies.sh
@@ -1,21 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-## python
-apt install -y \
-python3 python3-pip python3-venv
-
-## development
-apt install -y \
-cmake libgirepository-2.0-dev libdbus-1-dev
-
-## indi
-apt install -y \
-libcfitsio-dev libnova-dev libindi-dev swig
-
-## virtualgps
-apt install -y \
-gpsd gpsd-tools apparmor-utils
-
-## yr_weather
-sudo apt install -y \
-libcairo2-dev gobject-introspection libgirepository1.0-dev
+apt update && apt install -y \
+python3 python3-pip python3-venv \
+libcairo2-dev gobject-introspection swig \
+libgirepository-2.0-dev libdbus-1-dev libindi-dev libcfitsio-dev libnova-dev
