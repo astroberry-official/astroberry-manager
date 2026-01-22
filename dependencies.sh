e4752db4e19d81151415e2ca9f22484032ea89c9
Radek Kaczorek <rkaczorek@gmail.com>
2026-01-22 11:53:31 +0100

    Update dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -5,6 +5,6 @@
 #
 
 apt update && apt install -y \
-python3 python3-pip python3-venv \
+python3 python-is-python3 python3-pip python3-venv \
 libcairo2-dev gobject-introspection swig \
 libgirepository-2.0-dev libdbus-1-dev libindi-dev libcfitsio-dev libnova-dev
